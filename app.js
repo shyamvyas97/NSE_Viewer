7cf3365798fb2fcfdca52ec690ab194b1b228ec2
Nirav Tilala <niravtilal3@gmail.com>
2018-07-09 10:37:11 +0530

    initialized project with dependencies

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port= 3000;
+const ejs = require('ejs');
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.use(function (req, res, next) {
+    res.status(404).send('404 - Not Found!');
+});
+
+app.listen(port, () => {
+    console.log('Example app listening on port :'+port);
+});
