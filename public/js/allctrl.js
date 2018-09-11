95b6ff36caaf62b6910a81c2a729c17b1fdc4875
Niravtilala <niravtilala3@gmail.com>
2018-09-11 14:32:15 +0530

    add chart

--- public/js/allctrl.js
+++ public/js/allctrl.js
@@ -7,12 +7,15 @@ myapp.controller('home', ["$scope", "$http", function ($scope, $http) {
             console.log(err);
         });
 }]);
+
+// myapp.controller('');
+
 myapp.controller('stock', ["$scope", "quandl", "$stateParams","$q", "DTOptionsBuilder","DTColumnBuilder", function ($scope, quandl, $stateParams,$q, DTOptionsBuilder,DTColumnBuilder) {
     $scope.dtOptions=DTOptionsBuilder.fromFnPromise(function() {
         var defer = $q.defer();
         quandl.request("https://www.quandl.com/api/v3/datasets/NSE/" + $stateParams.snm + "/data.json?")
         .then(function (resp) {
-            // $scope.data = resp.data;
+            $scope.data = resp.data.dataset_data.data;
             defer.resolve(resp.data.dataset_data.data);
         })
         .catch(function (err) {
@@ -30,5 +33,63 @@ myapp.controller('stock', ["$scope", "quandl", "$stateParams","$q", "DTOptionsBu
         DTColumnBuilder.newColumn(6).withTitle("Total Trade Quantity"),
         DTColumnBuilder.newColumn(7).withTitle("Turnover (Lacs)")   
     ];
-    
+    // $scope.date=[];
+    // $scope.open=[];
+    // $scope.high=[];
+    // $scope.low=[];
+    // $scope.last=[];
+    // $scope.close=[];
+    // $scope.ttq=[];
+    // $scope.tuonover=[];
+    // for (var i = 0; i < $scope.data.length; i++) {
+    //     $scope.data.push($scope.data[i][0]);
+    //     $scope.open.push($scope.data[i][1]);
+    //     $scope.high.push($scope.data[i][2]);
+    //     $scope.low.push($scope.data[i][3]);
+    //     $scope.last.push($scope.data[i][4]);
+    //     $scope.close.push($scope.data[i][5]);
+    //     $scope.ttq.push($scope.data[i][6]);
+    //     $scope.tuonover.push($scope.data[i][7]);
+    // }
+
+    // $scope.labels = $scope.data;  
+    // $scope.data1=[$scope.open,$scope.high,$scope.low,$scope.last,$scope.close,$scope.ttq,$scope.tuonover];
+    // $scope.series = ['Open', 'High','low','last','close','Total Trading quantity','Tuonover'];
+
+}]);
+
+
+myapp.controller('chart', ["$scope", "quandl", "$stateParams","$q", function ($scope, quandl, $stateParams,$q) {
+        var defer = $q.defer();
+        quandl.request("https://www.quandl.com/api/v3/datasets/NSE/" + $stateParams.snm + "/data.json?")
+        .then(function (resp) {
+            $scope.data1 = resp.data.dataset_data.data;
+            $scope.date=[];
+            $scope.open=[];
+            $scope.high=[];
+            $scope.low=[];
+            $scope.last=[];
+            $scope.close=[];
+            $scope.ttq=[];
+            $scope.tuonover=[];
+            // console.log($scope.data1);
+            for (var i = $scope.data1.length-1; i>=0; i--) {
+                $scope.date.push($scope.data1[i][0]);
+                $scope.open.push($scope.data1[i][1]);
+                $scope.high.push($scope.data1[i][2]);
+                $scope.low.push($scope.data1[i][3]);
+                $scope.last.push($scope.data1[i][4]);
+                $scope.close.push($scope.data1[i][5]);
+                $scope.ttq.push($scope.data1[i][6]);
+                $scope.tuonover.push($scope.data1[i][7]);
+            }
+        
+            $scope.labels = $scope.date;  
+            $scope.data=$scope.open;
+            $scope.series = ['Open', 'High','low','last','close','Total Trading quantity','Tuonover'];
+
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
 }]);
--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-var myapp = angular.module('stock', ['ui.router','datatables','datatables.bootstrap']);
+var myapp = angular.module('stock', ['ui.router', 'chart.js','datatables','datatables.bootstrap']);
 
 myapp.config(['$stateProvider', '$urlRouterProvider','$qProvider', function ($stateProvider, $urlRouterProvider,$qProvider) {
     $qProvider.errorOnUnhandledRejections(false);
@@ -12,6 +12,11 @@ myapp.config(['$stateProvider', '$urlRouterProvider','$qProvider', function ($st
             url: "/stock/:snm",
             templateUrl: 'views/stock.html',
             controller: 'stock'
+        })
+        .state('chart', {
+            url: "/chart/:snm",
+            templateUrl: 'views/chart.html',
+            controller: 'chart'
         })
     $urlRouterProvider.otherwise('/');
 
